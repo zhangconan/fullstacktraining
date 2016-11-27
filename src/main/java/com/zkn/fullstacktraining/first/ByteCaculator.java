bbd5467eb66fd8e044efc2dad82815e7c6bb7b71
ConanZhang <zhangconancomputer@126.com>
2016-11-27 18:43:07 +0800

    初始化项目

---
+++ src/main/java/com/zkn/fullstacktraining/first/ByteCaculator.java
@@ -0,0 +1,15 @@
+package com.zkn.fullstacktraining.first;
+
+/**
+ * Created by zkn on 2016/11/27.
+ * 位运算是针对整形数据的。
+ * byte类型：8位一个字节。
+ *
+ */
+public class ByteCaculator {
+
+    public static void main(String[] args){
+        byte aa = 127;
+        System.out.println(Byte.MAX_VALUE);
+    }
+}
