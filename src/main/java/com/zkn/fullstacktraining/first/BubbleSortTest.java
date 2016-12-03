7e3d7788a95e7bb214b2bc8d51be644eeab2c6b8
ConanZhang <zhangconancomputer@126.com>
2016-12-04 01:31:00 +0800

    新增冒泡排序

---
+++ src/main/java/com/zkn/fullstacktraining/first/BubbleSortTest.java
@@ -0,0 +1,34 @@
+package com.zkn.fullstacktraining.first;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by zkn on 2016/12/4.
+ */
+public class BubbleSortTest {
+
+    public static void main(String[] args){
+        int[] a = {3,1,10,9,100,7,4,-2,6,2};
+        testBubbleSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    /**
+     * 冒泡排序
+     * @param salaries
+     */
+    private static void testBubbleSort(int[] salaries) {
+        for(int i=0;i<salaries.length-1;i++){
+            for(int j=i;j<salaries.length;j++){
+                int tmp = 0;
+                if(salaries[i] < salaries[j]){
+                    tmp = salaries[i];
+                    salaries[i] = salaries[j];
+                    salaries[j] = tmp;
+                }
+            }
+        }
+    }
+}
