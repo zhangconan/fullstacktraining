05d43e0aeca3ef087a4a7ad59cd999ad784fa61a
ConanZhang <zhangconancomputer@126.com>
2016-12-16 00:40:21 +0800

    用FileChannel的方式将对象写入文件

---
+++ src/main/java/com/zkn/fullstacktraining/second/GenSalary.java
@@ -0,0 +1,26 @@
+package com.zkn.fullstacktraining.second;
+
+import com.zkn.fullstacktraining.first.RandomString;
+import com.zkn.fullstacktraining.first.Salary;
+
+import java.util.Random;
+
+/**
+ * Created by zkn on 2016/12/15.
+ */
+public class GenSalary {
+
+    private static Random random = new Random();
+
+    public static Salary[] getSalary(){
+        Salary[] salaries = new Salary[100];
+        for(int i=0;i<salaries.length;i++){
+            Salary salary = new Salary();
+            salary.setName(RandomString.getRamdomString(5));
+            salary.setBaseSalary(random.nextInt(10000000));
+            salary.setBonus(random.nextInt(10000000));
+            salaries[i] = salary;
+        }
+        return salaries;
+    }
+}
