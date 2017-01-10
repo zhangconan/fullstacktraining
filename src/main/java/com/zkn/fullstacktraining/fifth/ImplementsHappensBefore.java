e59da83d66900ff53078f59525da73e62590c30c
zknxx <zhangconancomputer@126.com>
2017-01-10 16:30:35 +0800

    新增实现Happens-Before的几种方式

--- src/main/java/com/zkn/fullstacktraining/fifth/ImplementsHappensBefore.java
+++ src/main/java/com/zkn/fullstacktraining/fifth/ImplementsHappensBefore.java
@@ -2,7 +2,14 @@ package com.zkn.fullstacktraining.fifth;
 
 /**
  * Created by wb-zhangkenan on 2017/1/10.
- *
+ * 有几种方式能实现多个线程共享变量之间的happens-before方式
  */
 public class ImplementsHappensBefore {
+    /**
+     * 1、用syncronized 关键字。
+     * 2、用lock方法。
+     * 3、用volatile关键字。
+     * 4、用原子类型。在java.util.concurrent.atomic包下。如：AtomicInteger、AtomicDouble。
+     * 5、对于64位的类型，可以用LongAdder、DoubleAdder.
+     */
 }
