4f1af3547e295e911292eee672991250ba60bf24
zknxx <zhangconancomputer@126.com>
2017-01-16 18:08:34 +0800

    测试 总是将wait放在循环中.测试if出现的死锁、执行错误的情况

--- src/main/java/com/zkn/fullstacktraining/sixth/ProducerAndConsumerModel02.java
+++ src/main/java/com/zkn/fullstacktraining/sixth/ProducerAndConsumerModel02.java
@@ -1,7 +1,60 @@
 package com.zkn.fullstacktraining.sixth;
 
+import java.util.stream.IntStream;
+
 /**
  * Created by wb-zhangkenan on 2017/1/16.
  */
 public class ProducerAndConsumerModel02 {
+
+    public static void main(String[] args){
+        ProducerAndConsumer producerAndConsumer = new ProducerAndConsumer();
+        /**
+         * 10个生产者。
+         */
+        IntStream.range(0,10).forEach((e)->{Thread thread = new Thread(()->producerAndConsumer.producer());thread.start();});
+        /**
+         * 10个消费者
+         */
+        IntStream.range(0,10).forEach((e)->{Thread thread = new Thread(()->producerAndConsumer.consumer());thread.start();});
+    }
+}
+class ProducerAndConsumer{
+
+    private static Object obj = new Object();
+    private boolean flag = false;
+
+    public void producer(){
+
+        synchronized (obj){
+            if (flag){
+                try {
+                    System.out.println("1111生成者"+Thread.currentThread().getName());
+                    obj.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("生产者flag="+flag+"  "+Thread.currentThread().getName());
+            flag = true;
+            obj.notifyAll();
+        }
+    }
+
+    public void consumer(){
+
+        synchronized (obj){
+            if (!flag) {
+                try {
+                    System.out.println("2222消费者"+Thread.currentThread().getName());
+                    obj.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("消费者flag="+flag+"  "+Thread.currentThread().getName());
+            flag = false;
+            obj.notifyAll();
+        }
+    }
 }
